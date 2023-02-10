a33c9126c86f65ddfb9ed3f00b4ea4a03c292067
maniac-O <cj45645@naver.com>
2023-02-10 20:38:01 +0900

    [feat] : update all

--- src/main/java/com/prjfoody/foody/controller/BasketController.java
+++ src/main/java/com/prjfoody/foody/controller/BasketController.java
@@ -1,2 +1,85 @@
-package com.prjfoody.foody.controller;public class BasketController {
+package com.prjfoody.foody.controller;
+
+import com.prjfoody.foody.domain.Basket;
+import com.prjfoody.foody.domain.Product;
+import com.prjfoody.foody.domain.Users;
+import com.prjfoody.foody.parser.UserFromRequest;
+import com.prjfoody.foody.service.Services;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+@Slf4j
+public class BasketController implements Controllers<Basket> {
+
+    private final Services<Basket> service;
+    private final UserFromRequest userFromRequest;
+
+    public BasketController(@Qualifier("BasketService") Services<Basket> service, UserFromRequest userFromRequest) {
+        this.service = service;
+        this.userFromRequest = userFromRequest;
+    }
+
+    @GetMapping("/basket")
+    @Override
+    public String select(Basket basket, Model model, HttpServletRequest request) {
+        Users sessionUser = userFromRequest.convert(request);
+        List<Basket> selectBasket = service.select(basket, sessionUser);
+        model.addAttribute("baskets", selectBasket);
+
+        return null;
+    }
+
+
+    @PostMapping("/basket/create")
+    public String createBasket(Product product, Model model, HttpServletRequest request) {
+        Users sessionUser = userFromRequest.convert(request);
+
+        // services interface를 상속하고 있기 때문에 무조건 basket을 넘겨줄수 밖에 없음
+        // 어쩔수 없이 new Basket을 해야하는 상황임
+        Basket basket = new Basket();
+        basket.setProduct(product);
+        service.create(basket, sessionUser);
+
+        return null;
+    }
+
+    @Override
+    public String create(Basket basket, Model model, HttpServletRequest request) {
+        return null;
+    }
+/*
+    @PostMapping("/basket/create")
+    @Override
+    public String create(Basket basket, Model model, HttpServletRequest request) {
+        Users sessionUser = userFromRequest.convert(request);
+        service.create(basket, sessionUser);
+        return null;
+    }
+
+
+ */
+    @PostMapping("/basket/update")
+    @Override
+    public String update(Basket basket, Model model, HttpServletRequest request) {
+        Users sessionUser = userFromRequest.convert(request);
+        service.update(basket, sessionUser);
+        return null;
+    }
+
+    @GetMapping("/basket/delete/{id}")
+    @Override
+    public String delete(@PathVariable Long id, HttpServletRequest request) {
+        Users sessionUser = userFromRequest.convert(request);
+        service.delete(id, sessionUser);
+        return null;
+    }
 }
--- src/main/java/com/prjfoody/foody/controller/ExceptionHandlerControllerAdvice.java
+++ src/main/java/com/prjfoody/foody/controller/ExceptionHandlerControllerAdvice.java
@@ -1,2 +1,35 @@
-package com.prjfoody.foody.controller;public class ExceptionHandlerControllerAdvice {
+package com.prjfoody.foody.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+@Slf4j
+public class ExceptionHandlerControllerAdvice {
+
+    @ExceptionHandler(NullPointerException.class)
+    public String handleNullPointerException() {
+        log.warn("NullPointerException Occur!!!");
+        return "main";
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handle404(){
+        log.warn("404 Not Found Occur!!!");
+
+        return "404";
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handle400(){
+        log.warn("400 Bad Request Occur!!!");
+
+        return "404";
+    }
 }
