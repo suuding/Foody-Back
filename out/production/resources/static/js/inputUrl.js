9712517e3e6e8300b05e7fe5f3e9ed5cfa18af6e
maniac-O <cj45645@naver.com>
2023-02-12 04:16:44 +0900

    Revert "이미지 파일 추가"
    This reverts commit 738be6f13cfa08c4e44f88ccb5172569eb56f845.

--- out/production/resources/static/js/inputUrl.js
+++
@@ -1,9 +0,0 @@
-let search_button = document.querySelector('#search-button');
-search_button.addEventListener('click', ()=>{
-    let getUrl = document.querySelector('#search-url');
-    alert('/product?title='+getUrl.value)
-
-    window.location.href='/product?title='+getUrl.value;
-})
-
-
